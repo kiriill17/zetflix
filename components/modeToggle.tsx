b9c44e2c12a80fe9a271db7e55b89250720f9b26
Kirill Zakharov <kiriill17@icloud.com>
2024-11-20 15:24:50 +0500

    first commit18

--- components/modeToggle.tsx
+++ components/modeToggle.tsx
@@ -18,7 +18,7 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
+        <Button className="hidden" variant="outline" size="icon">
           <Sun className="hidden h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="hidden absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="hidden sr-only">Toggle theme</span>
